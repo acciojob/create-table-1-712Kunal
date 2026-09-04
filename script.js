def767116d5829594104064b7d6b4e7875305574
Kunal Jadhav <155985737+712Kunal@users.noreply.github.com>
2026-09-05 01:59:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
   
-  
+  let table=document.getElementById("sampleTable");
+let row=document.createElement("tr");
+	let data1=document.createElement("td");
+	data1.innerText="New Cell1";
+
+	let data2=document.createElement("td");
+	data2.innerText="New Cell2";
+
+	row.appendChild(data1);
+	row.appendChild(data2);
+
+	table.appendChild(row);
 }
